fc4f17c25b94c9cb5a41e5dd7d46c940669a5a80
iqbalsalman <iqbalsalman1707@gmail.com>
2018-01-11 13:33:20 +0700

    Latihan Selections

---
+++ belajarsql1.java
@@ -0,0 +1,51 @@
+select employee_id as NIK, first_name AS "Nama Depan" ,last_name AS Namabelakang, first_name||' '|| last_name AS "Nama Lengkap" from employees 
+where first_name='Daniel'
+
+
+select 
+   employee_id,lower(first_name),le(last_name),salary,(salary/30) AS GAJI
+   from 
+       employees 
+
+
+      select  salary from employees where salary > 10000 and employee_id > 200
+
+           select  * from employees where salary > 10000 and employee_id > 200
+ 
+ select employee_id,first_name,last_name,salary
+ from
+     employess
+  where
+  employee_id not in(16,163)
+  and
+  employee_id in(100)
+
+  select *from employees 
+
+where first_name like 'A%' OR last_NAME LIKE 'B%'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
